c97f8e549252bce1618ccda1097f10cb0406c33e
bartosz-pigla <221072@student.pwr.edu.pl>
2017-12-23 23:39:28 +0100

    postgresql support

---
+++ src/main/resources/data-postgresql.sql
@@ -0,0 +1,11 @@
+INSERT INTO EMPLOYEE (EMPLOYEE_ID,FIRST_NAME,LAST_NAME,DATE_OF_EMPLOYMENT,LEADER_ID)
+VALUES
+  (1,'Anna','Zawiecka','1999-01-01',NULL),
+  (2,'Krystyna','Jacak','1990-01-01',NULL),
+  (3,'Wanda','Nowak','2001-11-11',1),
+  (4,'Katarzyna','Kowalski','2010-08-10',1),
+  (5,'Magdalena','Mazur','2005-03-14',2),
+  (6,'Małgorzata','Irecka','2009-09-06',5),
+  (7,'Jacek','Opińska','2001-11-02',3),
+  (8,'Mariusz','Zły','2011-11-25',3),
+  (9,'Krzysztof','Dobry','2012-04-21',8);
